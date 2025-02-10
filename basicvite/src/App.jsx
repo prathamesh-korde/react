dfcedb85be2972490b11110886910414c322b54b
prathamesh korde <kordeprathamesh01@gmail.com>
2025-02-10 16:16:14 +0530

    modification in basic vite project

--- basicvite/src/App.jsx
+++ basicvite/src/App.jsx
@@ -1,8 +1,13 @@
 
-
+import Yt from "./yt"
 function App() {
   return (
-    <h1> vit react app</h1>
+    <>
+      <h1> vit react app</h1>
+      /* we are able to writen 1 elament so we are able to use hack that return two element inside single one */
+      <h1> vit react app</h1>
+      <Yt/>
+    </>
   )
 }
 
---
+++ basicvite/src/yt.jsx
@@ -0,0 +1,6 @@
+function Yt (){
+    return(
+        <h1>youtube app</h1>
+    )
+}
+export default Yt;
