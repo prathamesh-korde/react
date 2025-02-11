874a58960400cba7812575f766cbb04eedcf0d93
prathamesh korde <kordeprathamesh01@gmail.com>
2025-02-11 11:26:08 +0530

    modularizing structure

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,9 +7,22 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+let object = {
+  name:'prathamesh korde',
+  age:20,
+  address:{
+    city:'Nagpur',
+    staate:'Maharastra',
+    country:'India'
+  }
+}
+let newArr=[1,2,3,4,5,6,7,8,9];
+
   return (
     <>
       <h1 className='text-3xl bg-green-500 p-3 rounded-md' >Vite with Tailwind</h1>
+      <Card username="pratahamesh" myArr={newArr}/>
+      <Card username='pratham' post="staff Engg"/>
       <Card/>
     </>
   )
