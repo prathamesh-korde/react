0b9abaddfdee3e4c48f6e2b383257f9a713cdb95
prathamesh-korde <kordeprathamesh01@gmail.com>
2025-08-11 22:13:26 +0530

    React Propes

--- Basic-React-app/src/Blog.jsx
+++ Basic-React-app/src/Blog.jsx
@@ -1,9 +1,11 @@
 import './Blog.css';
-function Blog() {
+function Blog({title,price=2000, features, features2={a:"responsive", b:"cost effective", c:"easy-to-use"}}) {
   return (
     <div className='blog-container'>
-      <h1>Welcome to My Blog</h1>
-      <p>This is a simple blog application built with React</p>
+      <h1>{title}</h1>
+      <p>Price:{price/2}</p>
+      <p>features : {features}</p>
+      <p>features2 :{features2.a}</p>
     </div>
   );
 }
--- Basic-React-app/src/BlogContainer.jsx
+++ Basic-React-app/src/BlogContainer.jsx
@@ -1,12 +1,18 @@
 import Blog from "./blog";
 
 function BlogContainer() {
+  //let options = [<li>"highh-quality"</li>, <li>"affordable"</li>, <li>"durable"</li>];
+  let options = ["high-quality", "affordable", "durable"];
+   //let list = options.map((feature)=><li>{feature}</li>);
+  let option2 = {a:"fast", b:"responsive", c:"easy-to-use"};
+ 
+
   return (
     <div>
-        <Blog />
-      <Blog />
-      <Blog />
-    </div>
+        <Blog title="phone" price={15000} features={options.map((feature)=><li>{feature}</li>)} features2={option2}/>
+        <Blog title="laptop" price={50000}/>
+        <Blog title="tab" />     
+     </div>
   );
 }
 
